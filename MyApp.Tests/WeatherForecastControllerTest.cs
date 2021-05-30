e2d92b552befe57ec77ffaff742e5cf33f372e09
Mahabubul Hasan <codehasan@gmail.com>
2021-05-30 23:21:49 +0600

    Add a sample unit test

--- MyApp.Tests/WeatherForecastControllerTest.cs
+++ MyApp.Tests/WeatherForecastControllerTest.cs
@@ -1,4 +1,7 @@
-using System;
+using Microsoft.Extensions.Logging;
+using Moq;
+using MyApp.Controllers;
+using System.Collections.Generic;
 using Xunit;
 
 namespace MyApp.Tests
@@ -6,9 +9,14 @@ namespace MyApp.Tests
     public class WeatherForecastControllerTest
     {
         [Fact]
-        public void Test1()
+        public void Get_ShouldReturn_Array()
         {
+            var logger = new Mock<ILogger<WeatherForecastController>>();
+            var controller = new WeatherForecastController(logger.Object);
 
+            Assert.NotNull(controller.Get());
+
+            
         }
     }
 }
